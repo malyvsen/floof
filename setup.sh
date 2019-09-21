99b8f26b9693ce65907d777bd2a634676acd4b6b
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-09-21 15:58:07 +0200

    fix typo in neofetch invocation

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@
 (source ./setup/yay.sh) &&
 (source ./setup/tools.sh) &&
 (source ./setup/config.sh) &&
-neofetch --source "$(xdg-user-dir WALLPAPERS)/desret-wolf/dark.png" --w3m &&
+neofetch --source "$(xdg-user-dir WALLPAPERS)/desert-wolf/dark.png" --w3m &&
 echo "[desert-wolf] setup finished succesfully. you may want to look through ${HOME}/.config"
