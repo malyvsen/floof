38bcc3bf445c97d9523ed9900b3742b340b7e0fd
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-09-24 19:30:51 +0200

    recolor ttys

--- home/.config/bash/colors.sh
+++ home/.config/bash/colors.sh
@@ -1,1 +1,2 @@
-cat ~/.cache/wal/sequences
+# colors-tty.sh checks if it's a TTY and returns 1 if it isn't - then assume it's a terminal
+source ~/.cache/wal/colors-tty.sh || cat ~/.cache/wal/sequences
--- setup/config.sh
+++ setup/config.sh
@@ -3,5 +3,5 @@ source "$(dirname ${BASH_SOURCE[0]})/utils.sh" &&
 rice_message "setting up config files" &&
 cp -r "$(dirname ${BASH_SOURCE[0]})/../home/." "${HOME}/" &&
 xrdb ~/.Xresources &&
-wal -t -i "$(xdg-user-dir WALLPAPERS)/$RICE_NAME.jpg" &&
+wal -i "$(xdg-user-dir WALLPAPERS)/$RICE_NAME.jpg" &&
 source ~/.bashrc
