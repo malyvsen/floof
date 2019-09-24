b87210e21597e627a03455d8ebfb6c3dc12e5d35
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-09-24 20:36:52 +0200

    add showoff alias

--- home/.config/bash/aliases.sh
+++ home/.config/bash/aliases.sh
@@ -4,4 +4,5 @@ alias media="cd /run/media/$USER"
 alias userlist="cut -d: -f1 /etc/passwd"
 alias psgrep="ps aux | grep -v grep | grep -i -e VSZ -e"
 alias eensh="scrot -s -o ~/temp/screenshot.png"
+alias showoff="neofetch --w3m"
 eval $(thefuck --alias)
