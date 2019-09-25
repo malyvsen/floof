f6dd2edc394793029d2181e5c1d2645c518c5b1c
Nicholas Bochenski <5940672+malyvsen@users.noreply.github.com>
2019-09-25 13:08:10 +0200

    add missing yay dependencies

--- setup/yay.sh
+++ setup/yay.sh
@@ -1,7 +1,7 @@
 source "$(dirname ${BASH_SOURCE[0]})/utils.sh" &&
 
 rice_message "installing yay" &&
-sudo pacman -S binutils expac yajl &&
+sudo pacman -S gcc make binutils expac yajl fakeroot &&
 cd "${REPOS_DIR}" &&
 git clone https://aur.archlinux.org/yay.git &&
 cd yay &&
