a8396ca4895296402d1cf71d4de4047301db58a4
Nicholas Bochenski <5940672+malyvsen@users.noreply.github.com>
2019-09-25 13:01:56 +0200

    add binutils dependency to yay install script

--- setup/yay.sh
+++ setup/yay.sh
@@ -1,7 +1,7 @@
 source "$(dirname ${BASH_SOURCE[0]})/utils.sh" &&
 
 rice_message "installing yay" &&
-sudo pacman -S expac yajl &&
+sudo pacman -S binutils expac yajl &&
 cd "${REPOS_DIR}" &&
 git clone https://aur.archlinux.org/yay.git &&
 cd yay &&
