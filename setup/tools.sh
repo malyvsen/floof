e1c8bae0bec16aa70c811bde4e0e6f710ff1c0d1
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-09-25 19:00:08 +0200

    add dmenu

--- setup/tools.sh
+++ setup/tools.sh
@@ -5,6 +5,6 @@ yay -S --needed python python-pip python2 &&
 pip install thefuck --user &&
 yay -S --needed python-pywal feh imagemagick &&
 yay -S --needed ttf-google-fonts-git &&
-yay -S --needed ranger neofetch gotop-bin sysstat &&
+yay -S --needed dmenu ranger neofetch gotop-bin sysstat &&
 yay -S --needed atom-editor-bin firefox &&
 yay -S --needed scrot xclip pslist
