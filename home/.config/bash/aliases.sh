40514002ef28f944686b871e1926fac3f74180ed
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-09-27 13:04:04 +0200

    add highlight alias

--- home/.config/bash/aliases.sh
+++ home/.config/bash/aliases.sh
@@ -1,6 +1,7 @@
 alias home="cd ~"
 alias media="cd /run/media/$USER"
 alias list_users="cut -d: -f1 /etc/passwd"
+alias highlight="grep --color=always"
 alias psgrep="ps aux | grep -v grep | grep -i -e VSZ -e"
 alias eensh="scrot -s -o ~/temp/screenshot.png"
 alias show_off="neofetch --w3m"
