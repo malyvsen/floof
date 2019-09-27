50bec1454f46b6a5754f6b713cd9dda8e2d8dfc4
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-09-27 13:02:08 +0200

    use snake case in aliases

--- home/.config/bash/aliases.sh
+++ home/.config/bash/aliases.sh
@@ -1,8 +1,8 @@
 alias ..="cd .."
 alias home="cd ~"
 alias media="cd /run/media/$USER"
-alias userlist="cut -d: -f1 /etc/passwd"
+alias list_users="cut -d: -f1 /etc/passwd"
 alias psgrep="ps aux | grep -v grep | grep -i -e VSZ -e"
 alias eensh="scrot -s -o ~/temp/screenshot.png"
-alias showoff="neofetch --w3m"
+alias show_off="neofetch --w3m"
 eval $(thefuck --alias)
