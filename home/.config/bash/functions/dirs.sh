67938610f8e866b1daafbc8b063a5abaaa630fbb
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-09-27 12:02:37 +0200

    more intuitive up function

--- home/.config/bash/functions/dirs.sh
+++ home/.config/bash/functions/dirs.sh
@@ -1,16 +1,12 @@
-# jump directories upwards until it hits a directory with multiple folders
+# jump directories upwards until a directory beginning with $1 is reached
 function up {
-  local d=""
-  limit=$1
-  for ((i=1 ; i <= limit ; i++))
-    do
-      d=$d/..
-    done
-  d=$(echo $d | sed 's/^\///')
-  if [ -z "$d" ]; then
-    d=..
-  fi
-  cd $d
+  while [[ ! "$PWD" == "/" ]]; do
+    cd ..
+    if [[ "$(basename "$PWD")" =~ ^$1.* ]]; then
+      return 0
+    fi
+  done
+  return 1
 }
 
 # create a directory and directly cd into it
