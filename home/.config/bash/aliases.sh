fa44346aa88cc4eb0c2fe27d0b0cff89902927dd
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-10-04 14:02:50 +0200

    organize keyboard shortcuts

--- home/.config/bash/aliases.sh
+++ home/.config/bash/aliases.sh
@@ -3,6 +3,5 @@ alias media="cd /run/media/$USER"
 alias list_users="cut -d: -f1 /etc/passwd"
 alias highlight="grep --color=always"
 alias psgrep="ps aux | grep -v grep | grep -i -e VSZ -e"
-alias eensh="scrot -s -o ~/temp/screenshot.png"
 alias show_off="neofetch --w3m"
 eval $(thefuck --alias)
