340b2cea6faf43e82dfc28d6df8db812d412007d
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-10-09 22:53:21 +0200

    refactor (simplify) setup.sh

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@
 
 source "$(dirname ${BASH_SOURCE[0]})/setup/utils.sh" &&
 
-(source "$(dirname ${BASH_SOURCE[0]})/setup/dirs.sh") &&
-(source "$(dirname ${BASH_SOURCE[0]})/setup/yay.sh") &&
-(source "$(dirname ${BASH_SOURCE[0]})/setup/tools.sh") &&
-(source "$(dirname ${BASH_SOURCE[0]})/setup/config.sh") &&
-neofetch --source "$(xdg-user-dir WALLPAPERS)/$RICE_NAME.jpg" --w3m &&
+source "$(dirname ${BASH_SOURCE[0]})/setup/dirs.sh" &&
+source "$(dirname ${BASH_SOURCE[0]})/setup/yay.sh" &&
+source "$(dirname ${BASH_SOURCE[0]})/setup/tools.sh" &&
+source "$(dirname ${BASH_SOURCE[0]})/setup/config.sh" &&
+show_off &&
 rice_message "setup finished succesfully. you may want to look through ${HOME}/.config"
