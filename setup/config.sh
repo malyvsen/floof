94f7e0b23dfa22bf41800b6ea1c339ad9b1cc060
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-10-12 16:33:11 +0200

    move cursor to config

--- setup/config.sh
+++ setup/config.sh
@@ -1,6 +1,7 @@
 source "$(dirname ${BASH_SOURCE[0]})/utils.sh" &&
 
 if rice_ask "set up config files?"; then
+  yay -S --needed bibata-cursor-theme &&
   cp -r "$(dirname ${BASH_SOURCE[0]})/../home/." "${HOME}/" &&
   xrdb ~/.Xresources &&
   wal -i "$(xdg-user-dir WALLPAPERS)/$RICE_NAME.jpg" &&
--- setup/cursor.sh
+++
@@ -1,5 +0,0 @@
-source "$(dirname ${BASH_SOURCE[0]})/utils.sh" &&
-
-if rice_ask "install cursor theme?"; then
-  yay -S --needed bibata-cursor-theme
-fi
