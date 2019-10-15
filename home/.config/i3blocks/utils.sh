3b2bf4ce86deda00cd05e4c7dccefc5320b0c0f4
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-10-15 22:54:47 +0200

    configure application-specific workspaces

--- home/.config/i3blocks/utils.sh
+++ home/.config/i3blocks/utils.sh
@@ -1,5 +1,5 @@
 # launch terminal in temporary workspace, executing given command
 function launch_terminal {
-  i3-msg -q "workspace temp"
+  i3-msg -q "workspace 13:temp"
   i3-sensible-terminal -e "sh -c \"cat ~/.cache/wal/sequences && $1\""
 }
