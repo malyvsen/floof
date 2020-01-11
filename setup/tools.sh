5734192392f3c813ba5dc55b2c9c0ddf5eeb5271
malyvsen <5940672+malyvsen@users.noreply.github.com>
2020-01-11 20:37:32 +0100

    add ntp to tools

--- setup/tools.sh
+++ setup/tools.sh
@@ -8,5 +8,6 @@ if rice_ask "install additional tools?"; then
   yay -S --needed dmenu ranger autojump-git &&
   yay -S --needed neofetch gotop-bin sysstat pslist &&
   yay -S --needed atom-editor-bin firefox &&
-  yay -S --needed bat exa mediainfo scrot xclip
+  yay -S --needed bat exa mediainfo scrot xclip &&
+  yay -S --needed ntp && sudo systemctl enable ntpd.service
 fi
